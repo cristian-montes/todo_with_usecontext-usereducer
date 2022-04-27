06f565e4e868ac46d2fc4e786cab75b43192acf5
Cristian Montes <cmontesmujica@gmail.com>
2022-04-26 21:51:08 -0600

    added functions to the todoState to delete,toggle and add todos

--- src/context/TodoState.jsx
+++ src/context/TodoState.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import React, {useReducer} from 'react'
+import { ADD_TODO, DELETE_TODO, TOGGLE_TODO } from './todo-actions'
 import TodoContext from './todo-context'
+import todoReducer from './todo-reducer'
 
 const TodoState = (props) => {
+    const initialState = {
+        todos:[]
+    }
+
+    const [state, dispatch] = useReducer(todoReducer, initialState)
+
+    //ADD TODO
+    const addTodo = (todo) => {
+        dispatch({
+            type: ADD_TODO,
+            payload: todo
+        })
+    }
+
+    // TOGGLE A TODO
+    const toggleTodo = (todoID) => {
+        dispatch({
+            type: TOGGLE_TODO,
+            payload: todoID
+        })
+    }
+
+    //DELETE A TODO
+    const deleteTodo = (todoID) => {
+        dispatch({
+            type: DELETE_TODO,
+            payload: todoID
+        })
+    }
+
+
   return (
-    <TodoContext.Provider value={{}}>
+    <TodoContext.Provider value={{
+        todos: state.todos,
+        addTodo,
+        toggleTodo,
+        deleteTodo
+    }}>
         {props.children}
     </TodoContext.Provider>
   )
